9c790913b05640ff516549a960f46ac8f9e52f38
Numa Alset <oo2126630oo@gmail.com>
2024-09-15 04:02:08 +0300

    Add SSR and CSR

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,17 +1,15 @@
 import { Fragment } from 'react';
-import { useRouter } from 'next/router';
 
-import { getEventById } from '../../dummy-data';
+
 import EventSummary from '../../components/event-detail/event-summary';
 import EventLogistics from '../../components/event-detail/event-logistics';
 import EventContent from '../../components/event-detail/event-content';
 import ErrorAlert from '../../components/ui/error-alert';
+import { getAllEvents, getEventsById } from '../../helpers/api-utils';
 
-function EventDetailPage() {
-  const router = useRouter();
+function EventDetailPage (props) {
 
-  const eventId = router.query.eventId;
-  const event = getEventById(eventId);
+  const event = props.selectedEvent;
 
   if (!event) {
     return (
@@ -36,5 +34,27 @@ function EventDetailPage() {
     </Fragment>
   );
 }
+
+export async function getStaticProps(context) {
+  const eventId = context.params.eventId;
+  const event = await getEventsById(eventId);
+  return{
+    props:{
+        selectedEvent : event
+    },
+    revalidate:30
+  };
+}
+
+export async function getStaticPaths() {
+  const events = await getAllEvents();
+  const paths = events.map(event => ({params : {eventId:event.id}}));
+  return{
+    paths:paths,
+    fallback: false
+  };
+}
+
+
 
 export default EventDetailPage;
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,22 @@
-import { getFeaturedEvents } from '../dummy-data';
 import EventList from '../components/events/event-list';
+import { getFeaturedEvents } from '../helpers/api-utils';
 
-function HomePage() {
-  const featuredEvents = getFeaturedEvents();
+function HomePage(props) {
+   
 
   return (
     <div>
-      <EventList items={featuredEvents} />
+      <EventList items={props.featuredEvents} />
     </div>
   );
 }
-
+export async function getStaticProps() {
+ const events = await getFeaturedEvents()
+  return {
+    props:{
+      featuredEvents: events
+    },
+    revalidate:1800
+  };
+}
 export default HomePage;
